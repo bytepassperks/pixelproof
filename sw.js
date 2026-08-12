b6d14f5ab3bf268e00fb1a167a7646d46a8783f1
bytepassperks <learn@skillmatch.co.site>
2026-08-12 22:37:23 +0530

    Upgrade hero cutout asset

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pixelproof-shell-20260812-34";
+const CACHE = "pixelproof-shell-20260812-35";
 const SHELL = [
   "./",
   "./index.html",
@@ -30,7 +30,8 @@ const SHELL = [
   "./manifest.webmanifest",
   "./favicon.svg",
   "./social-preview.svg",
-  "./vendor/images/pixelproof-cat-cutout.webp",
+  "./vendor/images/pixelproof-dog-hero-1024.webp",
+  "./vendor/images/pixelproof-dog-hero-512.webp",
   "./vendor/images/pixelproof-sample-photo-cutout.png",
   "./vendor/images/pixelproof-sample-photo.jpg",
   "./vendor/images/pixelproof-comparison-photo.jpg",
